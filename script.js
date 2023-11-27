88c01393d24e2508c13936d20f90572278047ac5
Mathankumar-93 <mathaneros93@gmail.com>
2023-11-27 12:26:11 +0530

    Call back task completed

---
+++ script.js
@@ -0,0 +1,37 @@
+setTimeout(() => {
+  document.getElementById("content").innerText = "10";
+  setTimeout(() => {
+    document.getElementById("content").innerText = "9";
+    setTimeout(() => {
+      document.getElementById("content").innerText = "8";
+      setTimeout(() => {
+        document.getElementById("content").innerText = "7";
+        setTimeout(() => {
+          document.getElementById("content").innerText = "6";
+          setTimeout(() => {
+            document.getElementById("content").innerText = "5";
+            setTimeout(() => {
+              document.getElementById("content").innerText = "4";
+              setTimeout(() => {
+                document.getElementById("content").innerText = "3";
+                setTimeout(() => {
+                  document.getElementById("content").innerText = "2";
+                  setTimeout(() => {
+                    document.getElementById("content").innerText = "1";
+                    setTimeout(() => {
+                      document.getElementById("content").innerText = "0";
+                      setTimeout(() => {
+                        document.getElementById("content").innerText =
+                          "HAPPY INDEPENDENCE DAY";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
